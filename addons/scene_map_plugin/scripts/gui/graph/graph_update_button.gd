1a41082e9110338c5651f6c6feb9c809c9a3e781
Xesa <fr.chesa@gmail.com>
2025-10-26 01:00:55 +0200

    fix: auto-updater bug

--- addons/scene_map_plugin/scripts/gui/graph/graph_update_button.gd
+++ addons/scene_map_plugin/scripts/gui/graph/graph_update_button.gd
@@ -4,19 +4,14 @@ extends Button
 ##
 ## A Button that opens the [SM_UpdateDialog] when pressed.
 
-const SM_UpdateDialog := preload(SceneMapConstants.UPDATE_DIALOG)
-
-var update_dialog : SM_UpdateDialog
-
 
 func _ready() -> void:
 	toggle_visibility(false)
-	update_dialog = SceneMapConstants.PANEL_REFERENCE.update_dialog
 	pressed.connect(_on_button_pressed)
 
 
 func _on_button_pressed() -> void:
-	update_dialog.toggle_visiblity(true)
+	SceneMapConstants.PANEL_REFERENCE.update_dialog.toggle_visiblity(true)
 
 
 func toggle_visibility(toggle : bool) -> void:
